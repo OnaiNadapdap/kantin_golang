d0e37a6203f888df9b75f186d7b316c4734a671f
Onai Nadapdap <onainadapdap1@gmail.com>
2024-05-27 02:29:02 +0700

    hide .env load file

--- config/config.go
+++ config/config.go
@@ -5,19 +5,19 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		panic("failed to load file")
-	}
-}
+// func LoadEnv() {
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		panic("failed to load file")
+// 	}
+// }
 
 type DBConfig struct {
 	Username string
